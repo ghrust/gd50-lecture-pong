1d08a24ff5601d19409c81f8a31e30538cc81cfa
Rustam Garaev <garaev.rustam.h@gmail.com>
2021-02-27 21:18:31 +0400

    detect ball collision with paddles

--- main.lua
+++ main.lua
@@ -71,6 +71,33 @@ function love.update(dt)
     Runs every frame, with "dt" passed in, our delta in seconds
     since the last frame, which LÖVE2D supplies us.
   ]]
+  if gameState == 'play' then
+    -- detect ball collision with paddles, reversing dx if true and
+    -- slightly increasing it, then altering the dy based on the position of collision
+    if ball:collides(player1) then
+      ball.dx = -ball.dx * 1.03
+      ball.x = player1.x + 5
+
+      -- keep velocity going in the same direction, but randomize it
+      if ball.dy < 0 then
+        ball.dy = -math.random(10, 150)
+      else
+        ball.dy = math.random(10, 150)
+      end
+    end
+
+    if ball:collides(player2) then
+      ball.dx = -ball.dx * 1.03
+      ball.x = player2.x - 4
+
+      -- keep velocity going in the same direction, but randomize it
+      if ball.dy < 0 then
+        ball.dy = -math.random(10, 150)
+      else
+        ball.dy = math.random(10, 150)
+      end
+    end
+  end
   -- player 1 movement
   if love.keyboard.isDown('w') then
     player1.dy = -PADDLE_SPEED
